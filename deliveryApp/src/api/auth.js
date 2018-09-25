0e3031887c49785c7590784935bb4f4d452e12bd
veeran-dev <veeran.ambalam@gmail.com>
2018-09-25 17:10:36 +0530

    Add files via upload

---
+++ deliveryApp/src/api/auth.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import axios from 'axios'
+import { Actions } from 'react-native-router-flux';
+import * as action from '../actions/auth-actions'
+import store from '../Store'
+
+export function verifyLogin(pwd){
+  console.log("verifyLogin");
+  store.dispatch(action.loading(true));
+  store.dispatch(action.loginFailed(false));
+  return axios.get('https://www.abcd.online/deler.php?request=1&password='+pwd+'')
+  .then(response => {
+    console.log(response.data);
+    store.dispatch(action.loading(false));
+    console.log(response.data['id_user']);
+    console.log(response.data['id_user']!=0);
+    if(response.data['id_user'] != undefined && response.data['id_user'] != 0){
+      store.dispatch(action.loginUser(response.data));
+      Actions.dash(); 
+    }
+    else{
+      store.dispatch(action.loginFailed(true));
+    }
+    return response
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+export function toDashboard(){
+  Actions.dash();
+}
